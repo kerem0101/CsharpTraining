e72a4865d1f4011305a8c76e81e97865a3d31c1b
Aziz Kerem Demir <demirazizkerem@gmail.com>
2025-08-14 00:38:15 +0300

    TryCatch class added

---
+++ CsharpTraining/Snippets/TryCatch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpTraining.Snippets
+{
+    internal class TryCatch
+    {
+        public static void Run()
+        {
+            int[] array = {1,2,3};
+
+            try
+            {
+                Console.Write("Choose the array index: ");
+                int index = Convert.ToInt32(Console.ReadLine()); 
+                Console.WriteLine($"array[index]: {array[index]}");
+            }
+            catch (Exception message) 
+            {
+                Console.WriteLine($"{message}");
+            }
+        }
+    }
+}
