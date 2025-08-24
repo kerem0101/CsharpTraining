5e9da836092f1efda63d6d937be447eec4439960
Aziz Kerem Demir <demirazizkerem@gmail.com>
2025-08-24 13:56:21 +0300

    BankAccount.cs added

---
+++ CsharpTraining/Snippets/BankAccount.cs
@@ -0,0 +1,43 @@
+﻿
+
+namespace CsharpTraining.Snippets
+{
+    internal class BankAccount
+    {
+        private decimal balance = 0;
+
+        public decimal Balance
+        {
+            get => balance;
+        }
+
+        public BankAccount(decimal balance)
+        {
+            this.balance = balance;
+        }
+        
+        public void Deposit(decimal amount)
+        {
+            if(amount <= 0)
+            {
+                throw new ArgumentException("Deposit amount should be positive number!");
+            }
+            balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Withdraw amount should be positive number!");
+            }
+            if (amount > balance) 
+            {
+                throw new InvalidOperationException("Insufficient funds!"); 
+
+            }
+            balance -= amount;
+        }
+
+    }
+}
