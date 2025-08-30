73d0ad746af35892233314a065947f6c25113d0a
Aziz Kerem Demir <demirazizkerem@gmail.com>
2025-08-30 12:16:32 +0300

    comment added

--- CsharpTraining/Snippets/EmailService.cs
+++ CsharpTraining/Snippets/EmailService.cs
@@ -16,7 +16,7 @@ namespace CsharpTraining.Snippets
             Disconnect();
 
         }
-
+        // ALL THE BELOW METHODS ARE PRIVATE -- THEY ARE NOT EXPOSED TO OTHER CLASSES. OTHER CLASSES JUST WANT TO SEND EMAILS, NO NEED FOR THEM TO SEE ALL THE COMPLEX DETAILS OF CONNECTING TO MAIL SERVER, AUTHENTICATING, DISCONNECTING.
         private void Connect()
         {
             Console.WriteLine("Connecting to email server...");
