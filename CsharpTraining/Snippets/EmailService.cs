d4394979c91148d5941cca369f0c8717b12e7d1d
Aziz Kerem Demir <demirazizkerem@gmail.com>
2025-08-30 12:14:27 +0300

    EmailService.cs added

---
+++ CsharpTraining/Snippets/EmailService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpTraining.Snippets
+{
+    internal class EmailService
+    {
+        public void SendEmail()
+        {
+            Connect();
+            Authenticate();
+            Console.WriteLine("Sending email...");
+            Disconnect();
+
+        }
+
+        private void Connect()
+        {
+            Console.WriteLine("Connecting to email server...");
+        }
+
+        private void Authenticate()
+        {
+            Console.WriteLine("Authanticating...");
+        }
+
+        private void Disconnect()
+        {
+            Console.WriteLine("Disconnect from email server...");
+        }
+    }
+}
